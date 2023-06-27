617d1731d06ee0d5a9f4fc84f620351487247800
claracanavese <claracanavese@gmail.com>
2023-06-27 12:24:20 +0200

    Update ODE.R

--- ODE.R
+++ ODE.R
@@ -34,7 +34,7 @@ switching_process1 <- function(t, state, parameters) {
 parameters <- c(lambda_min = 15, lambda_plus = 10, omega_min = 0.01, omega_plus = 0.1)
 
 state <- c(rho = 1, sigma = 1)
-times <- seq(0, 1, by = 0.001)
+times <- seq(0, 2, by = 0.001)
 out <- ode(y = state, times = times, func = switching_process1, parms = parameters)
 plot(out)
 
@@ -55,7 +55,8 @@ switching_process2 <- function(t, state, parameters) {
   })
 }
 state <- c(X = 1, Y = 0)
-times <- seq(0, 1, by = 0.01)
+times <- seq(0, 2, by = 0.01)
+parameters <- c(lambda_min = 15, lambda_plus = 10, omega_min = 0.01, omega_plus = 0.1)
 out2 <- ode(y = state, times = times, func = switching_process2, parms = parameters)
 plot(out2)
 
@@ -67,7 +68,7 @@ n_obs <- rpois(n = length(times),
 plot(n_obs ~ times, xlab = "Time", ylab = "Z-")
 points(times, out[,2], type = "l", lwd=2)
 
-saveRDS(out, file = paste0("./simulations_time/ode_[15_10_01].rds"))
+saveRDS(out2_df, file = paste0("./simulations_time/ode_[15_10_001].rds"))
 
 # (co)variances
 covariances <- function(t, state, parameters) {
@@ -75,7 +76,7 @@ covariances <- function(t, state, parameters) {
     dM1 <- lambda_min*M1 + omega_min*M2
     dM2 <- lambda_plus*M2 + omega_plus*M1
     dV1 <- 2*lambda_min*V1 + (alpha_min+beta_min)*M1 + 2*omega_min*C + omega_min*M2
-    dV2 <- 2*lambda_plus*V1 + (alpha_plus+beta_plus)*M1 + 2*omega_plus*C + omega_plus*M2
+    dV2 <- 2*lambda_plus*V2 + (alpha_plus+beta_plus)*M2 + 2*omega_plus*C + omega_plus*M1
     dC <- (lambda_min + lambda_plus)*C + omega_plus*V1 + omega_min*V2
     list(c(dM1, dM2, dV1, dV2, dC))
   })
@@ -98,21 +99,18 @@ ggplot() +
 
 ggplot(out_df, aes(x = t, y = sqrt(C))) + geom_line()
 
-out_df %>% rename(c("<Z->" = "M1","<Z+>" = "M2")) %>% melt(value.name = "Z", id = "t") %>% ggplot(aes(t,Z, col=variable)) + ylab("Z") + geom_line() +
-  scale_colour_manual(values=c(rgb(102,204,102,maxColorValue = 255),"#D5D139")) + 
-  theme(axis.text = element_text(size = 10), axis.title = element_text(size = 14))
-
 plotr1 <- out_df %>% mutate(R1 = M1/sqrt(V1)) %>% mutate(R2 = M2/sqrt(V2)) %>% 
   ggplot() +
-  geom_line(aes(x = t,y = R1), color = "red")
-  #geom_line(aes(x = t, y = R2), color = "blue")
+  geom_line(aes(x = t,y = R1), color = "red") +
+  ylim(0,2.8)
 plotd1 <- out_df %>% ggplot(aes(x = t, y = D1)) + geom_line(color = "red")
 plotm1 <- out_df %>% ggplot(aes(x = t, y = M1)) + geom_line(color = "red")
 plotd1
 
 plotr2 <- out_df %>% mutate(R1 = M1/sqrt(V1)) %>% mutate(R2 = M2/sqrt(V2)) %>%
   ggplot() +
-  geom_line(aes(x = t,y = R2), color = "blue")
+  geom_line(aes(x = t,y = R2), color = "blue") +
+  ylim(0,2)
 plotd2 <- out_df %>% ggplot(aes(x = t, y = D2)) + geom_line(color = "blue")
 plotm2 <- out_df %>% ggplot(aes(x = t, y = M2)) + geom_line(color = "blue")
 plotd2
