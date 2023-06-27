d099ab7f7311a1a649a24145dfcb2a7dce86b9d2
claracanavese <claracanavese@gmail.com>
2023-06-27 12:24:23 +0200

    Update run_stan.R

--- run_stan.R
+++ run_stan.R
@@ -1,15 +1,13 @@
 library(rstan)
-library(rstanarm)
-N <- 10
 
 time_simulation <- readRDS("./simulations_time/output_[20_20_001][0.8].rds")
 time_simulation <- time_simulation %>% filter("Z+" > 1000)
 samples = time_simulation[seq(400000,800000, by = 40000),]
 
-simulation_py <- read.csv("./GitHub/switching_process/Gillespy2/switching_results_1.csv") %>%
+simulation_py <- read.csv("./GitHub/switching_process/Gillespy2/15_10_001/switching_results_5.csv") %>%
   tibble::as_tibble()
 simulation_py <- simulation_py[,-1]
-t_samples = seq(0.7,1.0, by = 0.03) %>% round(., 2)
+t_samples = seq(0.7,1.0, by = 0.3/15) %>% round(., 2)
 samples = simulation_py %>% filter(simulation_py$time %in% t_samples)
 
 data_list <- list(
@@ -21,22 +19,32 @@ data_list <- list(
 )
 
 model <- rstan::stan_model("./GitHub/switching_process/regression.stan")
-fit <- rstan::sampling(model, data_list, init = list(list(lambda_minus = 15, lambda_plus=10, omega_plus = 0.1, omega_minus = 0.01),list(lambda_minus = 15, lambda_plus=10, omega_plus = 0.1, omega_minus = 0.01),list(lambda_minus = 15, lambda_plus=10, omega_plus = 0.1, omega_minus = 0.01),list(lambda_minus = 15, lambda_plus=10, omega_plus = 0.1, omega_minus = 0.01)), chains=4, warmup=3000, iter = 6000)
-# fit <- rstan::sampling(model, data_list, chains=4, warmup=3000, iter = 6000)
+# fit <- rstan::sampling(model, data_list, init = list(list(lambda_minus = 15, lambda_plus=10, omega_plus = 0.1, omega_minus = 0.01),list(lambda_minus = 15, lambda_plus=10, omega_plus = 0.1, omega_minus = 0.01),list(lambda_minus = 15, lambda_plus=10, omega_plus = 0.1, omega_minus = 0.01),list(lambda_minus = 15, lambda_plus=10, omega_plus = 0.1, omega_minus = 0.01)), chains=4, warmup=3000, iter = 6000)
+fit <- rstan::sampling(model, data_list, chains=4, warmup=3000, iter = 6000)
+print(fit, pars = c("lambda_minus","lambda_plus","omega_minus","omega_plus"))
 print(fit)
 
 rstan::extract(fit, pars=c("lambda_minus"))
-bayesplot::mcmc_areas(fit, pars = c("lambda_plus"))
 bayesplot::mcmc_areas(fit, pars = c("lambda_minus"))
+bayesplot::mcmc_areas(fit, pars = c("lambda_plus"))
 bayesplot::mcmc_areas(fit, pars = c("omega_minus"))
+bayesplot::mcmc_areas(fit, pars = c("omega_plus"))
+
 bayesplot::mcmc_trace(fit, pars = c("lambda_minus"))
+bayesplot::mcmc_trace(fit, pars = c("lambda_plus"))
+bayesplot::mcmc_trace(fit, pars = c("omega_plus"))
+bayesplot::mcmc_trace(fit, pars = c("omega_minus"))
 
 bayesplot::ppc_intervals(
   y = samples$z_minus,
   yrep = rstan::extract(fit, pars = c("pred_minus"))$pred_minus %>% as.matrix(),
   x = samples$time,
   prob = 0.5
-)
+) 
+
+ggplot(simulation_py,aes(x=time, y=z_minus)) + geom_line()
+
+
 
 bayesplot::ppc_intervals(
   y = samples$z_plus,
@@ -46,12 +54,26 @@ bayesplot::ppc_intervals(
 )
 
 posterior = as.data.frame(fit)
-print(posterior)
-x = posterior %>% dplyr::select(starts_with("pred"))
 
+x = posterior %>% dplyr::select(starts_with("pred_minus"))
+x <- x[nrow(x),]
+x <- reshape2::melt(x)
+# x[,-1]
+min_pred_df <- data.frame(time = t_samples, z_min_pred = x[,-1])
+ggplot(min_pred_df, aes(x = time, y = z_min_pred)) + geom_point()
 
+y = posterior %>% dplyr::select(starts_with("pred_plus"))
+y <- y[nrow(y),]
+y <- reshape2::melt(y)
+plus_pred_df <- data.frame(time = t_samples, z_plus_pred = y[,-1])
+ggplot(plus_pred_df, aes(x = time, y = z_plus_pred)) + geom_point()
 
+ggplot() +
+  geom_line(data = simulation_py, aes(x = time, y = z_minus),  color = "forestgreen") +
+  geom_point(data = min_pred_df, aes(x = time, y = z_min_pred)) +
+  xlim(0.5,1)
 
-Y <- rexp(6, 1/7)
-typeof(Y)
-Y
+ggplot() +
+  geom_line(data = simulation_py, aes(x = time, y = z_plus),  color = "forestgreen") +
+  geom_point(data = plus_pred_df, aes(x = time, y = z_plus_pred)) +
+  xlim(0.5,1)
