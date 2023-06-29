6d30322546d4f9f111b21175e5c0d0f3b9821481
claracanavese <claracanavese@gmail.com>
2023-06-29 16:46:12 +0200

    Update run_stan.R

--- run_stan.R
+++ run_stan.R
@@ -4,13 +4,14 @@ library(dplyr)
 library(ggplot2)
 library(reshape2)
 library(patchwork)
+library(bayesplot)
 
 time_simulation <- readRDS("./simulations_time/output_[20_20_001][0.8].rds")
 time_simulation <- time_simulation %>% filter("Z+" > 1000)
 samples = time_simulation[seq(400000,800000, by = 40000),]
 
-simulation_py <- read.csv("./GitHub/switching_process/Gillespy2/no_switching.csv") %>%
-  tibble::as_tibble()
+simulation_py <- read.csv("./GitHub/switching_process/Gillespy2/10_15_01/switching_results_1.csv") %>% tibble::as_tibble()
+# simulation_py <- read.csv("./GitHub/switching_process/Gillespy2/no_switching.csv") %>% tibble::as_tibble()
 simulation_py <- simulation_py[,-1]
 t_samples = seq(0.7 ,1.2, by = (1.2-0.7)/10) %>% round(., 2)
 samples = simulation_py %>% filter(simulation_py$time %in% t_samples)
@@ -26,15 +27,29 @@ data_list <- list(
 
 model <- rstan::stan_model("./GitHub/switching_process/regression.stan")
 # fit <- rstan::sampling(model, data_list, init = list(list(lambda_minus = 15, lambda_plus=10, omega_plus = 0.1, omega_minus = 0.01),list(lambda_minus = 15, lambda_plus=10, omega_plus = 0.1, omega_minus = 0.01),list(lambda_minus = 15, lambda_plus=10, omega_plus = 0.1, omega_minus = 0.01),list(lambda_minus = 15, lambda_plus=10, omega_plus = 0.1, omega_minus = 0.01)), chains=4, warmup=3000, iter = 6000)
-fit <- rstan::sampling(model, data_list, chains=4, warmup=3000, iter = 6000)
-print(fit, pars = c("lambda_minus","lambda_plus","omega_minus","omega_plus"))
+fit <- rstan::sampling(model, data_list, chains=4, warmup=5000, iter=10000, cores=4)
+
+bayesplot::mcmc_trace(fit, pars = c("lambda_minus","lambda_plus","omega_minus","omega_plus"))
+print(fit, pars = c("lambda_minus","lambda_plus","omega_minus","omega_plus"), digits_summary = 3)
 print(fit)
+
 
 rstan::extract(fit, pars=c("lambda_minus"))
 bayesplot::mcmc_areas(fit, pars = c("lambda_minus"))
 bayesplot::mcmc_areas(fit, pars = c("lambda_plus"))
 bayesplot::mcmc_areas(fit, pars = c("omega_minus"))
 bayesplot::mcmc_areas(fit, pars = c("omega_plus"))
+
+prior_lambda = ggplot() +
+  stat_function(fun=dgamma, args = list(shape = 8.5, rate = 1./1.8)) +
+  xlim(0,25) + ggtitle("Prior")
+prior_omega = ggplot() +
+  stat_function(fun=dcauchy, args = list(location = 0.05, scale = 0.08)) +
+  xlim(0,0.5) + ggtitle("Prior")
+
+ggplot() +
+  stat_function(fun=dcauchy, args = list(location = 0.05, scale = 0.1)) +
+  geom_vline(xintercept = 0.1) + xlim(0,0.25)
 
 bayesplot::mcmc_trace(fit, pars = c("lambda_minus"))
 bayesplot::mcmc_trace(fit, pars = c("lambda_plus"))
@@ -55,6 +70,29 @@ bayesplot::ppc_intervals(
 )
 
 posterior = as.data.frame(fit)
+
+# plot samples from PRIOR
+y_prior = posterior %>% dplyr::select(starts_with("y_prior"))
+y_prior <- reshape2::melt(y_prior)
+ggplot(y_prior) + geom_density(aes(x=value,y=after_stat(density))) + xlim(-0.2,0.2)#+ stat_function(fun=dgamma, args = list(shape = 8.5, rate = 1./1.8))
+
+# compare POSTERIOR and PRIOR
+posterior_lambda_min = posterior %>% ggplot() + geom_density(aes(x = lambda_minus, y = after_stat(density))) + ggtitle("Posterior") + xlim(0,25)
+posterior_lambda_plus = posterior %>% ggplot() + geom_density(aes(x = lambda_plus, y = after_stat(density))) + ggtitle("Posterior") + xlim(0,25)
+posterior_omega_min = posterior %>% ggplot() + geom_density(aes(x = omega_minus, y = after_stat(density))) + ggtitle("Posterior") + xlim(0,0.5)
+posterior_omega_plus = posterior %>% ggplot() + geom_density(aes(x = omega_plus, y = after_stat(density))) + ggtitle("Posterior")+ xlim(0,0.5)
+
+posterior_lambda_min / prior_lambda
+posterior_lambda_plus / prior_lambda
+posterior_omega_min / prior_omega
+posterior_omega_plus / prior_omega
+
+ggplot() +
+  geom_density(data = posterior, aes(x = lambda_minus, y = after_stat(density))) +
+  stat_function(fun=dgamma, args = list(shape = 8.5, rate = 1.8)) 
+
+posterior <- posterior[,1:4]
+mcmc_intervals(posterior,)
 
 x = posterior %>% dplyr::select(starts_with("pred_minus"))
 x <- x[nrow(x),]
@@ -78,3 +116,8 @@ ggplot() +
   geom_line(data = simulation_py, aes(x = time, y = z_plus),  color = "forestgreen") +
   geom_point(data = plus_pred_df, aes(x = time, y = z_plus_pred)) +
   xlim(0.5,1)
+
+
+
+       
+       
