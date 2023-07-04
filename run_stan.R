d1afdf47462d71d8228f42336dda2d7771b79566
claracanavese <claracanavese@gmail.com>
2023-07-04 18:12:49 +0200

    Update run_stan.R

--- run_stan.R
+++ run_stan.R
@@ -10,19 +10,19 @@ time_simulation <- readRDS("./simulations_time/output_[20_20_001][0.8].rds")
 time_simulation <- time_simulation %>% filter("Z+" > 1000)
 samples = time_simulation[seq(400000,800000, by = 40000),]
 
-simulation_py <- read.csv("./GitHub/switching_process/Gillespy2/no switch/no_switching_results_15_10_4.csv") %>% tibble::as_tibble()
+simulation_py <- read.csv("./GitHub/switching_process/Gillespy2/1.5_1.0_005/switching_results_1.5_1.0_avg.csv") %>% tibble::as_tibble()
 # simulation_py <- read.csv("./GitHub/switching_process/Gillespy2/no_switching.csv") %>% tibble::as_tibble()
 simulation_py <- simulation_py[,-1]
-t_samples = seq(0.7 ,1.2, by = (1.2-0.7)/10) %>% round(., 2)
+t_samples = seq(1.0 ,4.0, by = 0.2) %>% round(., 2)
 samples = simulation_py %>% filter(simulation_py$time %in% t_samples)
 samples = simulation_py[seq(71,121,by=5),]
 
 data_list <- list(
   n_times = nrow(samples),
   t = samples$time,
-  zminus = samples$z_minus,
-  zplus = samples$z_plus,
-  z0 = c(1,1)
+  zminus = as.integer(samples$z_minus),
+  zplus = as.integer(samples$z_plus),
+  z0 = c(1000,100)
 )
 
 model <- rstan::stan_model("./GitHub/switching_process/regression.stan")
@@ -34,22 +34,22 @@ print(fit, pars = c("lambda_minus","lambda_plus","omega_minus","omega_plus"), di
 print(fit, digits_summary = 1e-8)
 
 
-rstan::extract(fit, pars=c("lambda_minus"))
+rstan::extract(fit, pars=c("omega_plus"))
 bayesplot::mcmc_areas(fit, pars = c("lambda_minus"))
 bayesplot::mcmc_areas(fit, pars = c("lambda_plus"))
 bayesplot::mcmc_areas(fit, pars = c("omega_minus"))
 bayesplot::mcmc_areas(fit, pars = c("omega_plus"))
 
 prior_lambda = ggplot() +
-  stat_function(fun=dgamma, args = list(shape = 4., rate = 1./3.)) +
-  xlim(0,25) + ggtitle("Prior")
+  stat_function(fun=dgamma, args = list(shape = 2., rate = 1.)) +
+  xlim(0,5) + ggtitle("Prior")
 prior_omega = ggplot() +
-  stat_function(fun=dcauchy, args = list(location = 0.05, scale = 0.08)) +
-  xlim(-0.2,0.2) + ggtitle("Prior")
+  stat_function(fun=dgamma, args = list(shape = 2., rate = 1/0.01)) +
+  xlim(0.,0.2) + ggtitle("Prior")
 
 ggplot() +
-  stat_function(fun=dcauchy, args = list(location = 0.05, scale = 0.1)) +
-  geom_vline(xintercept = 0.1) + xlim(0,0.25)
+  stat_function(fun=dgamma, args = list(shape = 2., rate = 100.)) +
+  xlim(0.,0.2) + ggtitle("Prior")
 
 bayesplot::mcmc_trace(fit, pars = c("lambda_minus"))
 bayesplot::mcmc_trace(fit, pars = c("lambda_plus"))
@@ -77,8 +77,8 @@ y_prior <- reshape2::melt(y_prior)
 ggplot(y_prior) + geom_density(aes(x=value,y=after_stat(density))) + xlim(-0.2,0.2)#+ stat_function(fun=dgamma, args = list(shape = 8.5, rate = 1./1.8))
 
 # POSTERIOR vs PRIOR
-posterior_lambda_min = posterior %>% ggplot() + geom_density(aes(x = lambda_minus, y = after_stat(density))) + ggtitle("Posterior") + xlim(0,25)
-posterior_lambda_plus = posterior %>% ggplot() + geom_density(aes(x = lambda_plus, y = after_stat(density))) + ggtitle("Posterior") + xlim(0,25)
+posterior_lambda_min = posterior %>% ggplot() + geom_density(aes(x = lambda_minus, y = after_stat(density))) + ggtitle("Posterior") + xlim(0,5)
+posterior_lambda_plus = posterior %>% ggplot() + geom_density(aes(x = lambda_plus, y = after_stat(density))) + ggtitle("Posterior") + xlim(0,5)
 posterior_omega_min = posterior %>% ggplot() + geom_density(aes(x = omega_minus, y = after_stat(density))) + ggtitle("Posterior") + xlim(0,0.2)
 posterior_omega_plus = posterior %>% ggplot() + geom_density(aes(x = omega_plus, y = after_stat(density))) + ggtitle("Posterior") + xlim(0,0.2)
 
@@ -117,7 +117,42 @@ ggplot() +
   geom_point(data = plus_pred_df, aes(x = time, y = z_plus_pred)) +
   xlim(0.5,1)
 
+# SECOND MODEL
+data_list <- list(
+  n_times = nrow(samples),
+  z0 = c(1000,100,0,0,0),
+  t0 = simulation_py$time[1],
+  zminus = as.integer(samples$z_minus),
+  zplus = as.integer(samples$z_plus),
+  t = samples$time
+)
+model <- rstan::stan_model("./GitHub/switching_process/regressionODE.stan")
+fit <- rstan::sampling(model, data_list, chains=4, warmup=5000, iter=10000, cores=4)
 
+bayesplot::mcmc_trace(fit, pars = c("theta[1]", "theta[2]", "theta[3]", "theta[4]"))
+print(fit, pars = c("theta[1]", "theta[2]", "theta[3]", "theta[4]"), digits_summary = 3)
+print(fit) 
 
-       
-       
+bayesplot::ppc_intervals(
+  y = samples$z_minus,
+  yrep = rstan::extract(fit, pars = c("pred_minus"))$pred_minus %>% as.matrix(),
+  x = samples$time,
+  prob = 0.5
+) +
+  bayesplot::ppc_intervals(
+    y = samples$z_plus,
+    yrep = rstan::extract(fit, pars = c("pred_plus"))$pred_plus %>% as.matrix(),
+    x = samples$time,
+    prob = 0.5
+  )      
+  
+posterior = as.data.frame(fit)
+posterior_lambda_min = posterior %>% ggplot() + geom_density(aes(x = `theta[1]`, y = after_stat(density))) + ggtitle("Posterior") + xlim(0,5)
+posterior_lambda_plus = posterior %>% ggplot() + geom_density(aes(x = `theta[2]`, y = after_stat(density))) + ggtitle("Posterior") + xlim(0,5)
+posterior_omega_min = posterior %>% ggplot() + geom_density(aes(x = `theta[3]`, y = after_stat(density))) + ggtitle("Posterior") + xlim(0,0.2)
+posterior_omega_plus = posterior %>% ggplot() + geom_density(aes(x = `theta[4]`, y = after_stat(density))) + ggtitle("Posterior") + xlim(0,0.2)
+
+posterior_lambda_min / prior_lambda
+posterior_lambda_plus / prior_lambda
+posterior_omega_min / prior_omega
+posterior_omega_plus / prior_omega
